724e68793bb6faaca2bfab75ee6edf772a5c9b65
Josh Barnes <JBarnes49@gmail.com>
2020-09-02 18:06:30 -0700

    fixed some formatting oddities

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -11,27 +11,27 @@ export default class NotePageMain extends React.Component {
     this.props.history.push('/')
   }
 
-  render(){
-    const { notes=[] } = this.context
+  render() {
+    const { notes = [] } = this.context
     const { noteId } = this.props.match.params;
-    const note = findNote(notes, noteId) || { content: ''};
+    const note = findNote(notes, noteId) || { content: '' };
 
     return (
-    <section className='NotePageMain'>
-      <Note
-        id={note.id}
-        name={note.name}
-        modified={note.modified}
-        onDeleteNote={this.handleDeleteNote}
-      />
-      <div className='NotePageMain__content'>
-        {note.content.split(/\n \r|\n/).map((para, i) =>
-          <p key={i}>{para}</p>
-        )}
-      </div>
-    </section>
-  )
-        }
+      <section className='NotePageMain'>
+        <Note
+          id={note.id}
+          name={note.name}
+          modified={note.modified}
+          onDeleteNote={this.handleDeleteNote}
+        />
+        <div className='NotePageMain__content'>
+          {note.content.split(/\n \r|\n/).map((para, i) =>
+            <p key={i}>{para}</p>
+          )}
+        </div>
+      </section>
+    )
+  }
 }
 
 NotePageMain.defaultProps = {
